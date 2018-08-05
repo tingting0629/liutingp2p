28a95ed447fcd28866458e079aeec9b1593a5f88
tingting0629 <632001322@qq.com>
2018-08-05 15:14:26 +0800

    day02 公共样式抽取！！！

---
+++ template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<!--icon图标-->
+		<link rel="shortcut icon" href="static/images/favicon.ico" />
+		<!--视口兼容移动端-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!--引入bootstrap核心样式-->
+		<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css" />
+		<!--引入图标字体-->
+		<link rel="stylesheet" type="text/css" href="lib/fontawesome/css/fontawesome-all.min.css"/>	
+		
+		<!--自定义样式-->
+		<link rel="stylesheet" type="text/css" href="dist/css/min/index.min.css" />
+		<!--兼容低版本的浏览器-->
+		<!--[if lt IE 9]>
+	      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+
+	<body>
+		<!--顶部导航条-->
+		<?php
+		   include_once("./header.html");
+		?>
+
+		<!--模板内容部分-->
+		<div class="container">
+			<h1>模板内容部分</h1>
+		</div>
+				
+		<!--页脚-->
+		<?php
+		   include_once("./footer.html");
+		?>
+		
+	</body>
+	<!--引入jquery库-->
+	<script src="lib/jquery/jquery.js" type="text/javascript" charset="utf-8"></script>
+	<!--引入bootstrap核心js库-->
+	<script src="lib/bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+	<!--自定义特效-->
+    <script src="src/javascript/p2pEffects.js" type="text/javascript" charset="utf-8"></script>
+
+</html>
